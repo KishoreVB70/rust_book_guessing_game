94e1abc80f9adea4f75db4d2db3998b41988af84
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-07 11:50:18 +0530

    added code to generate a random number from 1 to 10(inclusive)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::io;
+use rand::Rng;
+
 fn main() {
     let mut guess = String::new();
+    let rand_num = rand::thread_rng().gen_range(1..=10);
+
     println!("Welcome to the guessing game, enter a number from 1 to 10");
 
     io::stdin().read_line(&mut guess).expect("Failed to get the input");
+    println!("generated number: {rand_num}");
     println!("You have guessed: {guess}");
 }
