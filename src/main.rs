72d10f3a8d8077d80a39100e2bdfccaec8a816e1
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-07 11:45:06 +0530

    wrote the rust code to obtain input from the user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut guess = String::new();
+    println!("Welcome to the guessing game, enter a number from 1 to 10");
+
+    io::stdin().read_line(&mut guess).expect("Failed to get the input");
+    println!("You have guessed: {guess}");
 }
